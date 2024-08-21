c6095c26cad8a4d2903f4c145f5f60d4b8c883f1
Reinaldo <reinaldo18junior@gmail.com>
2024-08-21 13:21:06 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Reinaldo18Jr.png" />
+    <Titulo fontSize={20}>Reinaldo Junior</Titulo>
+  </aside>
+)
+
+export default Sidebar
