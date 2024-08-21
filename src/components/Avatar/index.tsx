297d673844f9055fdf3c2dbba653c40f8d2a4be3
Reinaldo <reinaldo18junior@gmail.com>
2024-08-21 14:34:27 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/Reinaldo18Jr.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Reinaldo18Jr.png" />
-    <Titulo fontSize={20}>Reinaldo Junior</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Reinaldo Junior</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Reinaldo18Jr
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
